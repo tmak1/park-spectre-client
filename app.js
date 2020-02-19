8fe36b247cc7d7291d451ed8d5913d90dc50398f
Camilla Champion de Crespigny <cjcdc@icloud.com>
2020-02-19 11:32:59 +1100

    added how it works side down info

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 function GetMap() {
-    
     var map = new Microsoft.Maps.Map('#myMap', {
         center: new Microsoft.Maps.Location(-37.818555, 144.959076),
         zoom: 15
     });
-    
 }
+
+$('.toggle').click(function(e) {
+    e.preventDefault();
+    $('.about').slideToggle(350);
+});
