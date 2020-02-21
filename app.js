3d36cae862e4ad608ff928ab5740f00c3434e6e6
Camilla Champion de Crespigny <cjcdc@icloud.com>
2020-02-21 11:38:49 +1100

    fixed header layout

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// https://docs.microsoft.com/en-us/bingmaps/v8-web-control/creating-and-hosting-map-controls/creating-a-basic-map-control
-// may be we need axios to make http request
-// const url = 'http://localhost:4567/sensors'
 var map
 var bays
 var loginTime
@@ -78,22 +75,19 @@ var createPin = (bay) => {
 // check if bay is occupied
         var bayStatus;
         var bayColor;
-        // icon = '<?xml version="1.0" ?><svg height="24" version="1.1" width="24" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><path d="m12.031 1030.4c-3.8657 0-6.9998 3.1-6.9998 7 0 1.3 0.4017 2.6 1.0938 3.7 0.0334 0.1 0.059 0.1 0.0938 0.2l4.3432 8c0.204 0.6 0.782 1.1 1.438 1.1s1.202-0.5 1.406-1.1l4.844-8.7c0.499-1 0.781-2.1 0.781-3.2 0-3.9-3.134-7-7-7zm-0.031 3.9c1.933 0 3.5 1.6 3.5 3.5 0 2-1.567 3.5-3.5 3.5s-3.5-1.5-3.5-3.5c0-1.9 1.567-3.5 3.5-3.5z" fill="#c0392b"/><path d="m12.031 1.0312c-3.8657 0-6.9998 3.134-6.9998 7 0 1.383 0.4017 2.6648 1.0938 3.7498 0.0334 0.053 0.059 0.105 0.0938 0.157l4.3432 8.062c0.204 0.586 0.782 1.031 1.438 1.031s1.202-0.445 1.406-1.031l4.844-8.75c0.499-0.963 0.781-2.06 0.781-3.2188 0-3.866-3.134-7-7-7zm-0.031 3.9688c1.933 0 3.5 1.567 3.5 3.5s-1.567 3.5-3.5 3.5-3.5-1.567-3.5-3.5 1.567-3.5 3.5-3.5z" fill="#e74c3c" transform="translate(0 1028.4)"/></g></svg>'
-        // icon = 'red.png'
-        // icon = '<?xml version="1.0" standalone="no"?><svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="20.000000pt" height="20.000000pt" viewBox="0 0 20.000000 20.000000" preserveAspectRatio="xMidYMid meet"><g transform="translate(0.000000,20.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none"></g></svg>'
+    
         if (bay.status === "Present") {
-            bayStatus = "Occupied"
-            icon = 'red.png' 
+            icon = 'images/red.png' 
         } else {
-            bayStatus = "Available"
-            icon = 'green.png'     
+            icon = 'images/green.png'     
         }
+
+        bayStatus = getBayStatus(bay)
 
         var pin = new Microsoft.Maps.Pushpin({ latitude: bay.lat, longitude: bay.lon }, 
             {
                 anchor: new Microsoft.Maps.Point(10, 10),
                 icon: icon
-
             }
         );
 
